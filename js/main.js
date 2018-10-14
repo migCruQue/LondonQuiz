698ac96ed2b2fcbdcea0a5a461886c04fc75a83a
Miguel Cruz Quesada <miguelcruzquesada@iMac-de-Miguel.local>
2018-10-14 12:41:03 +0100

    display

--- js/main.js
+++ js/main.js
@@ -6,155 +6,264 @@ const resultsContainer = document.getElementById('results');
 
 const submitButton = document.getElementById('submit');
 
-myQuestions = [
-
-    {
-      question: 'How many people died in the Great Fire of London in 1666?',
-      answer: {
-          a: 14,249,
-          b: 320,
-          c: 6
-              },
-      correctAnswer: 'c'
-
-
-
+const myQuestions = [
+  {
+    question: "Who is the strongest?",
+    answers: {
+      a: "Superman",
+      b: "The Terminator",
+      c: "Waluigi, obviously"
     },
-
-    {
-      question: 'London boasts the oldest public museum in the world, founded in 1753. Which is it?',
-      answer: {
-          a: 	'The British Museum',
-          b: 'The Natural History museum',
-          c: 'The Tate Modern'
-              },
-      correctAnswer: 'c'
-
-
+    correctAnswer: "c"
+  },
+  {
+    question: "What is the best site ever created?",
+    answers: {
+      a: "SitePoint",
+      b: "Simple Steps Code",
+      c: "Trick question; they're both the best"
     },
-
-    {
-      question: '	When did the last execution in the Tower of London take place?',
-      answer: {
-          a: '1841',
-          b: 	'1641',
-          c: '1941'
-              },
-      correctAnswer: 'c'
-
-
-    }
-
+    correctAnswer: "c"
+  },
+  {
+    question: "Where is Waldo really?",
+    answers: {
+      a: "Antarctica",
+      b: "Exploring the Pacific Ocean",
+      c: "Sitting in a tree",
+      d: "Minding his own business, so stop asking"
+    },
+    correctAnswer: "d"
+  }
 ];
 
 
-function buildQuiz(){
 
-  //const output stores the HTML output.
+function buildQuiz(){
+  // we'll need a place to store the HTML output
   const output = [];
 
-  //for each question loop
+  // for each question...
   myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
 
-    (currentQuestion, questionNumber)  => {
-
-      //const answers stores the list of answers choices
+      // we'll want to store the list of answer choices
       const answers = [];
 
-      //and for each available answer...
-      for (letter in currentQuestion.answers) {
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
 
-        //add an HTML radio button
-        answer.push(
-
+        // ...add an HTML radio button
+        answers.push(
           `<label>
-
             <input type="radio" name="question${questionNumber}" value="${letter}">
-
             ${letter} :
-
             ${currentQuestion.answers[letter]}
-
           </label>`
+        );
+      }
 
-                    );
-
-              }
-
-
-      //add this question and its answer to the output
+      // add this question and its answers to the output
       output.push(
-
-          `<div class"question">${currentQuestion.question} </div>
-
-          <div class="answers">${answers.join('')} </div>`
-
-
-                );
-
-            }
-
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
       );
+    }
+  );
 
-      //combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join('');
-
-
-
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
 }
 
-
-
-
-
-function showResults(){
-
-
-  //stores answer containers from our quiz
-  const answerContainers = quizContainer.querySelectorAll('.answers');
-
-  //keep tracks of user's answers
-  let numberCorrect = 0;
-
-  //for each question...
-  myQuestions.forEach(
-
-    (currentQuestion, questionNumber)  => {
-
-      //find the selected answer
-      const answerContainers = answerContainers[questionNumber];
-
-      const selector = `input[name=question' +questionNumber+ ']:checked`;
-
-      const userAnswer = (answerContainers.querySelectorAll(selector)) || {}).value;
-
-      //if the answer is correct..
-      if (userAnswer === currentQuestion.correctAnswer){
-
-          //add the number of correct answers
-          numberCorrect++;
-
-          //color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
-
-        }  else {
-
-            //if the answer is wrong or blank
-            //color the answer red
-            answerContainers[questionNumber].style.color = 'red';
-
-        }
-
-      });
-
-      //show number of correct answers out of total
-      resultsContainer.innerHTML = numberCorrect + ' out of ' + myQuestions.length;
-
-}
-
-//display quiz right away
+// display quiz right away
 buildQuiz();
 
 
-//on submit, show results
-submitButton.addEventListener('click', showResults);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const quizContainer = document.getElementById('quiz');
+//
+// const resultsContainer = document.getElementById('results');
+//
+// const submitButton = document.getElementById('submit');
+//
+// const myQuestions = [
+//
+//     {
+//       question: 'How many people died in the Great Fire of London in 1666?',
+//       answer: {
+//           a: 14.249,
+//           b: 320,
+//           c: 6
+//               },
+//       correctAnswer: 'c'
+//
+//
+//
+//     },
+//
+//     {
+//       question: 'London boasts the oldest public museum in the world, founded in 1753. Which is it?',
+//       answer: {
+//           a: 	'The British Museum',
+//           b: 'The Natural History museum',
+//           c: 'The Tate Modern'
+//               },
+//       correctAnswer: 'c'
+//
+//
+//     },
+//
+//     {
+//       question: '	When did the last execution in the Tower of London take place?',
+//       answer: {
+//           a: '1841',
+//           b: 	'1641',
+//           c: '1941'
+//               },
+//       correctAnswer: 'c'
+//
+//
+//     }
+//
+// ];
+//
+//
+// function buildQuiz(){
+//
+//   //const output stores the HTML output.
+//   const output = [];
+//
+//   //for each question loop
+//   myQuestions.forEach(
+//
+//     (currentQuestion, questionNumber)  => {
+//
+//       //const answers stores the list of answers choices
+//       const answers = [];
+//
+//       //and for each available answer...
+//       for (letter in currentQuestion.answers) {
+//
+//         //add an HTML radio button
+//         answer.push(
+//
+//           `<label>
+//
+//             <input type="radio" name="question${questionNumber}" value="${letter}">
+//
+//             ${letter} :
+//
+//             ${currentQuestion.answers[letter]}
+//
+//           </label>`
+//
+//                     );
+//
+//               }
+//
+//
+//       //add this question and its answer to the output
+//       output.push(
+//
+//           `<div class"question">${currentQuestion.question} </div>
+//
+//           <div class="answers">${answers.join('')} </div>`
+//
+//
+//                 );
+//
+//             }
+//
+//       );
+//
+//       //combine our output list into one string of HTML and put it on the page
+//       quizContainer.innerHTML = output.join('');
+//
+//
+//
+// }
+//
+//
+//
+//
+//
+// function showResults(){
+//
+//
+//   //stores answer containers from our quiz
+//   const answerContainers = quizContainer.querySelectorAll('.answers');
+//
+//   //keep tracks of user's answers
+//   let numberCorrect = 0;
+//
+//   //for each question...
+//   myQuestions.forEach(
+//
+//     (currentQuestion, questionNumber)  => {
+//
+//       //find the selected answer
+//       const answerContainers = answerContainers[questionNumber];
+//
+//       const selector = `input[name=question' +questionNumber+ ']:checked`;
+//
+//       const userAnswer = (answerContainers.querySelectorAll(selector)) || {}).value;
+//
+//       //if the answer is correct..
+//       if (userAnswer === currentQuestion.correctAnswer){
+//
+//           //add the number of correct answers
+//           numberCorrect++;
+//
+//           //color the answers green
+//           answerContainers[questionNumber].style.color = 'lightgreen';
+//
+//         }  else {
+//
+//             //if the answer is wrong or blank
+//             //color the answer red
+//             answerContainers[questionNumber].style.color = 'red';
+//
+//         }
+//
+//       });
+//
+//       //show number of correct answers out of total
+//       resultsContainer.innerHTML = numberCorrect + ' out of ' + myQuestions.length;
+//
+// }
+//
+// //display quiz right away
+// buildQuiz();
+//
+//
+// //on submit, show results
+// submitButton.addEventListener('click', showResults);
