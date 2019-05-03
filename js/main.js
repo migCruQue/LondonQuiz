c4274b8d9ef1f427256cf7a6554863914b972e54
migCruQue <miguel.cruz.quesada@gmail.com>
2019-05-03 17:37:45 +0100

    starting from scratch

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const submitButton = document.getElementById('submit');
 const myQuestions = [
 
     {
-      question: 'How many people died in the Great Fire of London in 1666?',
+      question: '1-How many people died in the Great Fire of London in 1666?',
       answers: {
           a: 14.249,
           b: 320,
@@ -20,21 +20,84 @@ const myQuestions = [
 
 
     },
+    {
+      question: "2-Pop culture time. What hotel is shown in the opening of Spice Girl's song 'Wannabe'?",
+      answers: {
+          a: 'The Ritz',
+          b: 'The Wolseley',
+          c: 'St Pancras Hotel'
+              },
+      correctAnswer: 'c'
+
+
+
+    },
 
     {
-      question: 'London boasts the oldest public museum in the world, founded in 1753. Which is it?',
+      question: '3-Where did the Great Fire of London begin?',
+      answers: {
+          a: 'Drury Lane',
+          b: 'Fishamble Street',
+          c: 'Pudding Lane'
+              },
+      correctAnswer: 'c'
+
+
+
+    },
+
+    {
+      question: "4-What's the quickest route between Leicester Square and Piccadilly Circus?'",
+      answers: {
+          a: 'Piccadilly Line',
+          b: 'Walk',
+          c: 'Taxi'
+              },
+      correctAnswer: 'b'
+
+
+
+    },
+
+    {
+      question: '5-What is the correct stop for Abbey Road?',
+      answers: {
+          a: 	'Baker Street',
+          b: "St John's Wood",
+          c: 'Maida Vale'
+              },
+      correctAnswer: 'b'
+
+
+    },
+
+    {
+      question: '6-London boasts the oldest public museum in the world, founded in 1753. Which is it?',
       answers: {
           a: 	'The British Museum',
           b: 'The Natural History museum',
           c: 'The Tate Modern'
+              },
+      correctAnswer: 'a'
+
+
+    },
+
+    {
+      question: '7-What year were the Boris Bikes launched?',
+      answers: {
+          a: 	'2008',
+          b: '2009',
+          c: '2010'
               },
       correctAnswer: 'c'
 
 
     },
 
+
     {
-      question: '	When did the last execution in the Tower of London take place?',
+      question: '8-When did the last execution in the Tower of London take place?',
       answers: {
           a: '1841',
           b: 	'1641',
@@ -43,9 +106,46 @@ const myQuestions = [
       correctAnswer: 'c'
 
 
+    },
+
+    {
+      question: '9-How long did it take to build the Shard?',
+      answers: {
+          a: 	'2007-2012',
+          b: '2009-2013',
+          c: '2008-2012'
+              },
+      correctAnswer: 'a'
+
+
+    },
+
+    {
+      question: '10-The London Underground consists of how many stations?',
+      answers: {
+          a: 	'390',
+          b: '270',
+          c: '130'
+              },
+      correctAnswer: 'b'
+
+
     }
 
+
 ];
+
+//pagination
+
+// const previousButton = document.getElementById('previous');
+//
+// const nextButton = document.getElementById('next');
+//
+// const slides = document.querySelectorAll('.slide');
+//
+// let currentSlide = 0;
+
+///
 
 
 
@@ -86,9 +186,13 @@ function buildQuiz(){
       //add this question and its answer to the output
       output.push(
 
-          `<div class"question">${currentQuestion.question} </div>
+          `<div class="slide>
 
-          <div class="answers">${answers.join('')} </div>`
+            <div class"question">${currentQuestion.question} </div>
+
+            <div class="answers">${answers.join('')} </div>
+
+          </div>`
 
 
                 );
@@ -107,6 +211,67 @@ function buildQuiz(){
 
 ////display quiz right away
 buildQuiz();
+
+//
+// function showSlide(n){
+//
+//   slides[currentSlide].classList.remove('active-slide');
+//
+//   slides[n].classList.add('active-slide');
+//
+//   currentSlide = n;
+//
+//   if (currentSlide===0){
+//
+//       previousButton.style.display = 'none';
+//   }
+//
+//   else {
+//
+//       previousButton.style.display = 'inline-block';
+//
+//
+//   }
+//
+//   if (currentSlide===slides.length-1) {
+//
+//     nextButton.style.display = 'none';
+//
+//     submitButton.style.display = 'inline-block';
+//
+//   }
+//
+//   else {
+//
+//     nextButton.style.display = 'inline-block';
+//
+//     submitButton.style.display = 'none';
+//
+//   }
+//
+//
+// }
+
+// showSlide(0);
+//
+//
+// function showNextSlide(){
+//
+//   showSlide(currentSlide + 1);
+//
+// }
+//
+// function showPreviousSlide(){
+//
+//   showSlide(currentSlide - 1);
+//
+// }
+//
+// previousButton.addEventListener('click', showPreviousSlide);
+//
+// nextButton.addEventListener('click', showNextSlide);
+
+
 
 
 function showResults(){
