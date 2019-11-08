5e0394ab89840dddcfa6552927f9af76a6957a6b
migCruQue <miguel.cruz.quesada@gmail.com>
2019-11-08 17:34:59 +0000

    display iphone 7plus

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 
+
+
+
+
+function resizeElementHeight(element) {
+  var height = 0;
+  var body = window.document.body;
+  if (window.innerHeight) {
+      height = window.innerHeight;
+  } else if (body.parentElement.clientHeight) {
+      height = body.parentElement.clientHeight;
+  } else if (body && body.clientHeight) {
+      height = body.clientHeight;
+  }
+  element.style.height = ((height - element.offsetTop) + "px");
+}
+
+
+
+
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> CONSTANTS & LETS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 /* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
