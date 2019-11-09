6910d7c95adcbd25b5d8da0f3d101e37489d010e
migCruQue <miguel.cruz.quesada@gmail.com>
2019-11-09 19:34:27 +0000

    data moved to a different file

---
+++ js/data.js
@@ -0,0 +1,144 @@
+
+
+// ARRAY OF OBJECTS: QUESTION, POSSIBLE ANSWERS AND CORRECT ANSWER
+
+const myQuestions = [
+
+    {
+      question: '1-How many people died in the Great Fire of London in 1666?',
+      answers: {
+          a: 14.249,
+          b: 320,
+          c: 6
+              },
+      correctAnswer: 'c',
+      picLap: 'img/questions pics/laptop L/The-Great-Fire-of-London_golden.jpg',
+    },
+    {
+      question: "2-Pop culture time. What hotel is shown in the opening of Spice Girl's song 'Wannabe'?",
+      answers: {
+          a: 'The Ritz',
+          b: 'The Wolseley',
+          c: 'St Pancras Hotel'
+              },
+      correctAnswer: 'c',
+      picLap: 'img/questions pics/laptop L/spice-girls_golden.jpg',
+
+    },
+
+    {
+      question: '3-Where did the Great Fire of London begin?',
+      answers: {
+          a: 'Drury Lane',
+          b: 'Fishamble Street',
+          c: 'Pudding Lane'
+              },
+      correctAnswer: 'c',
+      picLap: 'img/questions pics/laptop L/greatfirestarting_golden.jpg',
+
+    },
+
+    {
+      question: "4-What's the quickest route between Leicester Square and Piccadilly Circus?'",
+      answers: {
+          a: 'Piccadilly Line',
+          b: 'Walk',
+          c: 'Taxi'
+              },
+      correctAnswer: 'b',
+      picLap: 'img/questions pics/laptop L/shard_golden.jpg',
+
+    },
+
+    {
+      question: '5-What is the correct stop for Abbey Road?',
+      answers: {
+          a: 	'Baker Street',
+          b: "St John's Wood",
+          c: 'Maida Vale'
+              },
+      correctAnswer: 'b',
+      picLap: 'img/questions pics/laptop L/abbey-road_golden.jpg',
+    },
+
+    {
+      question: '6-London boasts the oldest public museum in the world, founded in 1753. Which is it?',
+      answers: {
+          a: 	'The British Museum',
+          b: 'The Natural History museum',
+          c: 'The Tate Modern'
+              },
+      correctAnswer: 'a',
+      picLap: 'img/questions pics/laptop L/muse_dorsay_golden.jpg',
+    },
+
+    {
+      question: '7-What year were the Boris Bikes launched?',
+      answers: {
+          a: 	'2008',
+          b: '2009',
+          c: '2010'
+              },
+      correctAnswer: 'c',
+      picLap: 'img/questions pics/laptop L/bikes_golden.jpg',
+    },
+
+
+    {
+      question: '8-When did the last execution in the Tower of London take place?',
+      answers: {
+          a: '1841',
+          b: 	'1641',
+          c: '1941'
+              },
+      correctAnswer: 'c',
+      picLap: 'img/questions pics/laptop L/execution_golden.jpg',
+
+    },
+
+    {
+      question: '9-How long did it take to build the Shard?',
+      answers: {
+          a: 	'2007-2012',
+          b: '2009-2013',
+          c: '2008-2012'
+              },
+      correctAnswer: 'a',
+      picLap: 'img/questions pics/laptop L/shard_golden.jpg',
+    },
+
+    {
+      question: '10-The London Underground consists of how many stations?',
+      answers: {
+          a: 	'390',
+          b: '270',
+          c: '130'
+              },
+      correctAnswer: 'b',
+      picLap: 'img/questions pics/laptop L/london-underground_golden.jpg',
+    }
+
+
+];
+
+
+const myResults = [
+
+    {queenPic: `img/queen pics/golden/queen0.jpeg`,
+     queenSays: `The Queen is horrified about your lack of knowledge, she considers you a mere peasant and won't even look at you`,
+     picSrc: `img/awards pics/square/dunce.jpg`},
+
+    {queenPic: `img/Queen pics/golden/queen1.jpg.jpg`,
+     queenSays: `The queen is very impressed, so she will name you Member of the Order of the British Empire (MBE)`,
+     picSrc: `img/awards pics/square/mbe-medal.png`},
+
+    {queenPic: `img/Queen pics/golden/queen2.jpg`,
+    queenSays: `The queen is in love with you, so she will name you Officer of the Order of the British Empire (OBE)`,
+    picSrc: `img/awards pics/square/obe-medal.png`},
+
+    {queenPic: `img/Queen pics/golden/queen3.jpg.jpg`,
+    queenSays: `The queen just adores you , so she will name you Commander of the Order of the British Empire (CBE)`,
+    picSrc: `img/awards pics/square/cbe-medal.jpg`},
+
+
+];
