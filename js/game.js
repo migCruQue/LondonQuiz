67e3c00c2a2a4502ac7d3ab9ea2acd0acb490ddc
migCruQue <miguel.cruz.quesada@gmail.com>
2020-12-13 14:19:26 +0000

    remove interval + replace div backgrond-image with img element

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ const ordinal = document.getElementById('ordinal');
 const pictureQuestion = document.getElementById('pictureQuestion')
 const choices = Array.from(document.getElementsByClassName('answerOption'));
 const quizContainer = document.getElementById('quizContainer');
+const loading = document.getElementById('loading');
 const checkAnswer = document.getElementById('checkAnswer');
 const scoreText = document.getElementById('score');
 const emoji = document.getElementById('emoji');
@@ -16,7 +17,7 @@ let score = 0;
 let availableQuestions = [];
 let currentQuestion = {};
 let acceptingAnswers = false;
-let goToResultFlag = false;
+let pictureLoaded = false;
 let startTime = new Date();  // *   global variable to calculate the points based in the time the user takes to click the option button,
                              // *   I declare it globaly as it has to be accessed for more the one functions.
 
@@ -67,12 +68,10 @@ document.addEventListener("DOMContentLoaded", event => {
 
 // ! STARTGAME FUNCTION
 startGame = () => {
-  clearInterval(nIntervId);       // * this stops the setInterval that start the game
   questionCounter = 0;
   score = 0;
   availableQuestions = [...dbQuestions];
   getNewQuestion();
-  quizContainer.classList.remove("d-none");
 };
 
 // ! GETNEWQUESTION FUNCTION
@@ -80,29 +79,14 @@ getNewQuestion = () => {
 
   if(availableQuestions.length === 0 || questionCounter >= AMOUNT_QUESTIONS_QUIZ){	
     localStorage.setItem('totalPoints', score);
-    goToResultFlag = true;	
-    //* go to the end page	
-    setTimeout(() => {window.location.assign("results.html")}, 2000);	
+    setTimeout(() => {window.location.assign("results.html")}, 1950);      //* go to the end page
   } else {
-    startTime = new Date();
     questionCounter++;
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
 
-    // * Get the picture from firebase storage project LondonQuiz and set up the pictureQuestion src attr to the url reference.
-
-    let storage = firebase.storage();
-
-    let gsReference = storage.refFromURL(`gs://londonquiz-f8499.appspot.com/${currentQuestion['pic']}`);
-
-    gsReference.getDownloadURL()
-    .then(function(url) {pictureQuestion.setAttribute('style', `background-image: url('${url}'`);})
-    .catch(function(error) {
-    });
-
-    // * ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    // * Set the question and the possible answers
     question.innerHTML = `<span id="ordinal">${questionCounter}/${AMOUNT_QUESTIONS_QUIZ}</span> &nbsp${currentQuestion.question}`;
     
     choices.forEach( choice => {
@@ -110,10 +94,28 @@ getNewQuestion = () => {
         choice.innerText = currentQuestion["option" + number];  
     });
 
+    getImageQuestion(currentQuestion);
+
+    //at the begginning of the game hide the div show the first question.
+    if(availableQuestions.length === 10) { setTimeout(() => {loading.classList.add('hidden')}, 300);} 
+
     availableQuestions.splice(questionIndex, 1);
 
     acceptingAnswers = true;
     }
+
+}
+
+
+//* function helper to retrieve the image from the firebase database and set the background-image inline css property.
+function getImageQuestion (currentQuestion) {
+  let storage = firebase.storage();
+
+  let gsReference = storage.refFromURL(`gs://londonquiz-f8499.appspot.com/${currentQuestion['pic']}`);
+
+  gsReference.getDownloadURL()
+  .then(function(url) {pictureQuestion.setAttribute('src', `${url}`);})
+  .catch(function(error) {console.log(`an error happened when trying to access the image => ${error}`)});
 }
 
 // * CALCULATESCORE FUNCTION subtracs the time that user takes to answers the question to 10000 ms if the answer is correct.
@@ -144,13 +146,11 @@ choices.forEach(choice => {
       quizContainer.classList.add('hidden'); 
       getNewQuestion();
       
-      setTimeout(() => {
-        if(!goToResultFlag){
+      setTimeout(() => { 
           checkAnswer.classList.add('hidden'); 
           quizContainer.classList.remove('hidden');
-        }
-      }
-        , 2000);
+          startTime = new Date();
+      } , 2000);
   });
 });
 
@@ -158,10 +158,5 @@ choices.forEach(choice => {
 
 
 
-  //* check if dbQuestions has been fully populated every 500ms. When it has been fully populated it launchs startGame(). 
-  // let nIntervId = setInterval(() => {
-  //   if(dbQuestions.length === AMOUNT_QUESTIONS_QUIZ){startGame()}
-  // },
-  // 500);
 
-  setTimeout(() => {startGame()}, 1250);
+  setTimeout(() => {startGame()}, 500);
