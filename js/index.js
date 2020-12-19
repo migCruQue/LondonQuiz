32c7ee74e3913128a414f4ac19bbeb901f47cef8
migCruQue <miguel.cruz.quesada@gmail.com>
2020-12-19 19:14:06 +0000

    yellow btn

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 //* SET THE BODY HEIGHT TO THE Window.HEIGHT
 // const body = document.querySelector('body');
 // body.style.height = `${window.innerHeight}px`;
